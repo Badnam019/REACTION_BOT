34d608bb57bc3ee70128f68528d5763a00728197
Badnam019 <yadavxd746@gmail.com>
2024-12-12 13:39:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   telegramBotTokens: [
-    "7015056498:AAGcZa04jeVe9EvsjlfW_6P2k95D57sL8cY",  // Bot token 1
+    "7517623382:AAHn-YeqKhUc_fffz9ljNkgf_NXQgGmuSho",  // Bot token 1
     "6443871444:AAEqas0L2bJQtybywpDuq3zcycWpiZV83vo",  // Bot token 2
     "7463542222:AAF14OHfN8mh5rstHUI5L6IZRITjHahBJYQ",  // Bot token 3
     // Add more tokens as needed
